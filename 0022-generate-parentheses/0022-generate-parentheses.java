2cc78580ad1baba057100a35bc183304f1ef32df
harsh-791 <144247829+harsh-791@users.noreply.github.com>
2024-07-29 02:40:26 +0530

    Time: 1 ms (85.31%), Space: 43 MB (58.65%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,29 +1,24 @@
 class Solution {
     ArrayList<String> ans;
 
-    public void helper(int remOpen, int remClose, StringBuilder curr){
+    public void helper(int remOpen, int remClose, String curr){
         if(remOpen ==0 && remClose == 0){
             ans.add(curr.toString());
             return;
         }
 
         if(remOpen > 0){
-            curr.append("(");
-            helper(remOpen-1,remClose,curr);
-            curr.deleteCharAt(curr.length()-1);
+            helper(remOpen-1,remClose,curr+"(");
         }
 
         if(remClose > remOpen){
-            curr.append(")");
-            helper(remOpen,remClose-1,curr);
-            curr.deleteCharAt(curr.length()-1);
+            helper(remOpen,remClose-1,curr+")");
         }
     }
 
     public List<String> generateParenthesis(int n) {
         ans = new ArrayList<>();
-        StringBuilder curr = new StringBuilder();
-        helper(n,n,curr);
+        helper(n,n,"");
         return ans;
     }
 }
